58d06999bb5fc970a2d350e8ef835bf96fd5509c
Haomin <haominhe@users.noreply.github.com>
2019-12-31 01:01:18 -0800

    .gitignore created
    .gitignore created

--- EasyQuestionsNodeJS/BranchSums.js
+++ EasyQuestionsNodeJS/BranchSums.js
@@ -1,5 +1,22 @@
-// This is the class of the input root.
-// Do not edit it.
+/*
+Write a function that takes in a Binary Tree and returns a list of its branch sums (ordered from
+leftmost branch sum to rightmost branch sum). A branch sum is the sum of all values in a Binary
+Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends
+at any leaf node. Each Binary Tree node has a value stored in a property called "value" and two
+children nodes stored in properties called "left" and "right," respectively. Children nodes can either
+be Binary Tree nodes themselves or the None (null) value.
+
+Sample input:
+      1
+    /   \
+    2   3
+   / \   / \
+   4 5   6  7
+   /\ \
+   8 9 10
+
+Sample output: [15, 16, 18, 10, 11]
+*/
 class BinaryTree {
     constructor(value) {
       this.value = value;
@@ -10,9 +27,11 @@ class BinaryTree {
   
   function branchSums(root) {
     // Write your code here.
+    return [15, 16, 18, 10, 11];
   }
   
   // Do not edit the lines below.
   exports.BinaryTree = BinaryTree;
   exports.branchSums = branchSums;
+  
   
