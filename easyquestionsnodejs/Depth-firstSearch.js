9c827c212abeac3cf570afbd9229a495cc838407
Haomin <haominhe@users.noreply.github.com>
2020-04-06 17:17:48 -0700

    depthFirstSearch
    depthFirstSearch

--- easyquestionsnodejs/Depth-firstSearch.js
+++ easyquestionsnodejs/Depth-firstSearch.js
@@ -12,4 +12,60 @@ graph = A
    E  F      G H
      I J     K
 
+Sample Output:
+['A','B','E','F','I','J','C','D','G','K','H']
+
+
+O(v + e) time | O(v) space - where v is the number of vertices of the input graph and e is
+the number of edges of the input graph
+
  */
+
+
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
+
+    // Method 1
+    depthFirstSearch1(array) {
+        array.push(this.name);
+        if (this.children) {
+            for (let index in this.children) {
+                this.dfsHelper(array, this.children[index])
+            }
+        }
+        // console.log(array);
+        return array;
+    }
+
+    dfsHelper(array, kidchildren) {
+        if (kidchildren.length === 0) return;
+        const curName = kidchildren.name;
+        array.push(curName);
+        if (kidchildren.children.length) {
+            kidchildren.children.forEach((e) => {
+                this.dfsHelper(array, e)
+            })
+        }
+        return;
+    }
+
+    // Method 2
+    depthFirstSearch(array) {
+        array.push(this.name);
+        for (const kid of this.children) {
+            // go inside of kid's scope and call kid's function
+            kid.depthFirstSearch(array);
+        }
+        return array;
+    }
+}
+
+exports.Node = Node;
