66c029f3a0ade5121023c83943f6b0810a11044b
Haomin <haominhe@users.noreply.github.com>
2020-06-05 01:09:16 -0700

    commit

--- easyquestionsnodejs/LinkedListConstruction.js
+++ easyquestionsnodejs/LinkedListConstruction.js
@@ -1,5 +1,5 @@
 /** Linked List Construction
-Write a DoublyLinkedList class that has a head and a tail , both of
+Write a DoublyLinkedList class that has a head and a tail, both of
 which point to either a linked list Node or None / null . The class should
 support:
 - Setting the head and tail of the linked list.
@@ -23,3 +23,57 @@ remove(2): 4 <-> 1 <-> 5 <-> 6 // remove the existing node with value 2
 containsNodeWithVa1ue(5} : true
 
  */
+
+ // This is an input class. Do not edit.
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.prev = null;
+      this.next = null;
+    }
+  }
+  
+  // Feel free to add new properties and methods to the class.
+  class DoublyLinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+  
+    setHead(node) {
+      // Write your code here.
+    }
+  
+    setTail(node) {
+      // Write your code here.
+    }
+  
+    insertBefore(node, nodeToInsert) {
+      // Write your code here.
+    }
+  
+    insertAfter(node, nodeToInsert) {
+      // Write your code here.
+    }
+  
+    insertAtPosition(position, nodeToInsert) {
+      // Write your code here.
+    }
+  
+    removeNodesWithValue(value) {
+      // Write your code here.
+    }
+  
+    remove(node) {
+      // Write your code here.
+    }
+  
+    containsNodeWithValue(value) {
+      // Write your code here.
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.Node = Node;
+  exports.DoublyLinkedList = DoublyLinkedList;
+  
--- easyquestionsnodejs/test/LinkedListConstructionTest.js
+++ easyquestionsnodejs/test/LinkedListConstructionTest.js
@@ -1,4 +1,88 @@
 // Test Command:
 // npm test ./test/LinkedListConstructionTest.js
 
+const program = require('./LinkedListConstruction');
+const chai = require('chai');
 
+class TestNode {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+const Node = program.Node || TestNode;
+
+function getNodeValuesHeadToTail(linkedList) {
+  const values = [];
+  let node = linkedList.head;
+  while (node !== null) {
+    values.push(node.value);
+    node = node.next;
+  }
+  return values;
+}
+
+function getNodeValuesTailToHead(linkedList) {
+  const values = [];
+  let node = linkedList.tail;
+  while (node !== null) {
+    values.push(node.value);
+    node = node.prev;
+  }
+  return values;
+}
+
+function bindNodes(nodeOne, nodeTwo) {
+  nodeOne.next = nodeTwo;
+  nodeTwo.prev = nodeOne;
+}
+
+it('Test Case #1', function () {
+  const linkedList = new program.DoublyLinkedList();
+  const one = new Node(1);
+  const two = new Node(2);
+  const three = new Node(3);
+  const three2 = new Node(3);
+  const three3 = new Node(3);
+  const four = new Node(4);
+  const five = new Node(5);
+  const six = new Node(6);
+  bindNodes(one, two);
+  bindNodes(two, three);
+  bindNodes(three, four);
+  bindNodes(four, five);
+  linkedList.head = one;
+  linkedList.tail = five;
+
+  linkedList.setHead(four);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 2, 3, 5]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([5, 3, 2, 1, 4]);
+
+  linkedList.setTail(six);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 2, 3, 5, 6]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([6, 5, 3, 2, 1, 4]);
+
+  linkedList.insertBefore(six, three);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 2, 5, 3, 6]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([6, 3, 5, 2, 1, 4]);
+
+  linkedList.insertAfter(six, three2);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 2, 5, 3, 6, 3]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([3, 6, 3, 5, 2, 1, 4]);
+
+  linkedList.insertAtPosition(1, three3);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([3, 4, 1, 2, 5, 3, 6, 3]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([3, 6, 3, 5, 2, 1, 4, 3]);
+
+  linkedList.removeNodesWithValue(3);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 2, 5, 6]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([6, 5, 2, 1, 4]);
+
+  linkedList.remove(two);
+  chai.expect(getNodeValuesHeadToTail(linkedList)).to.deep.equal([4, 1, 5, 6]);
+  chai.expect(getNodeValuesTailToHead(linkedList)).to.deep.equal([6, 5, 1, 4]);
+
+  chai.expect(linkedList.containsNodeWithValue(5)).to.deep.equal(true);
+});
